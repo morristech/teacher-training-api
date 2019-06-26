61ce3cb1ea725b58967f3895f16e93a188e3295c
Jake Benilov <benilov@gmail.com>
2019-06-26 08:55:43 +0100

    Optimise the Azure blob storage upload to make it 20x faster

--- upload-allocations.sh
+++ upload-allocations.sh
@@ -5,13 +5,7 @@ files=$(ls public/*.xlsx)
 echo "Getting access key"
 access_key=`az storage account keys list --account-name sadfebatallocations --subscription "DFE BAT Development" --query "[?keyName == 'key1'].value | [0]"`;
 
-for f in $files
-do
-  echo "Uploading $f"
 
-  filename=$(echo $f | sed 's/public\///g')
+az storage blob upload-batch --destination find-allocations --source ./public --pattern "*.xlsx"  --connection-string "DefaultEndpointsProtocol=https;AccountName=sadfebatallocations;AccountKey=$access_key;EndpointSuffix=core.windows.net" --verbose
 
-  az storage blob upload --container-name find-allocations --name "$filename" --file "$f" --connection-string "DefaultEndpointsProtocol=https;AccountName=sadfebatallocations;AccountKey=$access_key;EndpointSuffix=core.windows.net" > /dev/null
-
-  echo "Link is: https://sadfebatallocations.blob.core.windows.net/find-allocations/$filename"
-done
+echo "Link is: https://sadfebatallocations.blob.core.windows.net/find-allocations/filename"
